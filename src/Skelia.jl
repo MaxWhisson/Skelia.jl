d88ca27e561db40ea8ecfc7b34fc85a839d917b9
220001100 <mw307@st-andrews.ac.uk>
2025-09-03 15:09:52 +0100

    idk why CI is failing, iT wOrKs oN mY mAcHiNe

--- src/Skelia.jl
+++ src/Skelia.jl
@@ -88,7 +88,7 @@ end
 
 function run(inputs::Channel, outputs::Channel, data::AbstractArray, collector::Function)
     resultsChannel = Channel(Inf)
-    @spawn collector(length(data), outputs, resultsChannel)
+    @Threads.spawn collector(length(data), outputs, resultsChannel)
     zip(1:length(data), data) .|> (item -> put!(inputs, item))
     res = take!(resultsChannel)
     put!(inputs, QUIT())
@@ -103,7 +103,7 @@ end
 
 function buildSkel(s::Seq, dest::Channel; inChan = false)
     inputChannel = inChan == false ? Channel(Inf) : inChan
-    @spawn worker(s.f, inputChannel, dest)
+    @Threads.spawn worker(s.f, inputChannel, dest)
     return inputChannel
 end
 
@@ -123,7 +123,7 @@ end
 function buildSkel(skeleton::Feedback, dest::Channel; inChan = false)
     feedIn = inChan == false ? Channel(Inf) : inChan
     innerIn = buildSkel(skeleton.inner, feedIn)
-    @spawn create_feedback(skeleton.predicate, feedIn, dest, innerIn)
+    @Threads.spawn create_feedback(skeleton.predicate, feedIn, dest, innerIn)
     return innerIn
 end
 
